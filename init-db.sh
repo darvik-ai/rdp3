458d50952b49685ab32462460aad002e7e29bbb6
darvik-ai <159222738+darvik-ai@users.noreply.github.com>
2025-09-06 18:16:20 +0530

    Update init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cat /guacamole-schema/postgresql/*.sql | psql -h localhost -U guac_user -d guacamole_db
+export PGPASSWORD=$POSTGRES_PASSWORD
+echo "Applying Guacamole schema..."
+cat /guacamole-schema/postgresql/*.sql | psql -h localhost -U guac_user -d guacamole_db || {
+    echo "Error: Failed to apply Guacamole schema"
+    return 1
+}
